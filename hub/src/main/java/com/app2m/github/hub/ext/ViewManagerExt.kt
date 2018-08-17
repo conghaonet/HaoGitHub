b5d7fb2be8db5ea5a43efa85aab7e9e21bc405a8
hao.cong <hao.cong@app2m.com>
2018-08-18 00:58:17 +0800

    移除kotlin-kapt，需要时再添加

--- hub/src/main/java/com/app2m/github/hub/ext/ViewManagerExt.kt
+++ hub/src/main/java/com/app2m/github/hub/ext/ViewManagerExt.kt
@@ -4,7 +4,11 @@ import android.support.v7.widget.Toolbar
 import android.view.ViewManager
 import org.jetbrains.anko.custom.ankoView
 
-inline fun ViewManager.supportToolbar() = supportToolbar {}
+/**
+ * https://stackoverflow.com/questions/40604446/what-are-the-possible-values-that-can-be-given-to-suppress-in-kotlin
+ */
+@Suppress("NOTHING_TO_INLINE")
+inline fun ViewManager.supportToolbar(): Toolbar = supportToolbar() {}
 /**
  * SwipyRefreshLayout.()：T.()->Unit 和 ()->Unit 的区别
  * https://www.jianshu.com/p/88a656e59c61
