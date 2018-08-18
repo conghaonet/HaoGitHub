8ebb10038d3263cb80654de4e8d1674edac13139
hao.cong <hao.cong@app2m.com>
2018-08-18 23:09:13 +0800

    add MyApplication

---
+++ app/src/main/java/com/app2m/github/MyApplication.kt
@@ -0,0 +1,26 @@
+package com.app2m.github
+
+import android.content.res.Configuration
+import android.support.multidex.MultiDexApplication
+
+class MyApplication : MultiDexApplication() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+    }
+
+    override fun onTrimMemory(level: Int) {
+        super.onTrimMemory(level)
+    }
+}
