5334dd57525a2d261a76f2768b53450d22af2dee
conghao <hao.cong@app2m.com>
2018-08-24 17:23:51 +0800

    move to https package

--- network/src/main/java/com/app2m/github/network/https/HttpsTrustManager.java
+++ network/src/main/java/com/app2m/github/network/https/HttpsTrustManager.java
@@ -1,4 +1,4 @@
-package com.app2m.github.network;
+package com.app2m.github.network.https;
 
 import android.annotation.SuppressLint;
 
--- network/src/main/java/com/app2m/github/network/https/Tls12SocketFactory.java
+++ network/src/main/java/com/app2m/github/network/https/Tls12SocketFactory.java
@@ -1,4 +1,4 @@
-package com.app2m.github.network;
+package com.app2m.github.network.https;
 
 import android.os.Build;
 import android.util.Log;
