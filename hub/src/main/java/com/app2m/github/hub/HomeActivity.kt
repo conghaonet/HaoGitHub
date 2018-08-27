01467ff0661df40873967a12513b396ad4cfdc1a
hao.cong <hao.cong@app2m.com>
2018-08-28 01:45:30 +0800

    优化navigationView的headerView创建方式

--- hub/src/main/java/com/app2m/github/hub/HomeActivity.kt
+++ hub/src/main/java/com/app2m/github/hub/HomeActivity.kt
@@ -6,10 +6,7 @@ import android.support.design.widget.NavigationView
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.Toolbar
-import android.view.Gravity
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
+import android.view.*
 import com.app2m.github.hub.base.BaseActivity
 import com.app2m.github.hub.ext.themeSupportToolbar
 import com.app2m.github.network.GitHubService
@@ -18,13 +15,10 @@ import com.app2m.github.network.schedule
 import org.jetbrains.anko.*
 import org.jetbrains.anko.design.navigationView
 import org.jetbrains.anko.support.v4.drawerLayout
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.subscribeBy
-import io.reactivex.schedulers.Schedulers
 import org.jetbrains.anko.sdk25.coroutines.onClick
-
 
 class HomeActivity: BaseActivity() {
     private lateinit var homeActivityUI: HomeActivityUI
@@ -59,6 +53,7 @@ class HomeActivity: BaseActivity() {
             homeActivityUI.drawerLayout.closeDrawer(Gravity.START)
             true
         }
+
     }
     fun requestGitHubApi() {
         val apiService = RequestClient.buildService(GitHubService::class.java)
@@ -71,22 +66,6 @@ class HomeActivity: BaseActivity() {
                 }
         )
         mCompositeDisposable.add(disposable!!)
-
-/*
-        var disposable = apiService.getGitHubApi().onBackpressureDrop().schedule().subscribe(
-                {
-                    toast(it.toString())
-                }, {
-                    toast(it.getStackTraceString())
-                }, {
-                    toast("onComplete")
-                }, {
-                    it.request(Long.MAX_VALUE)
-                    toast("Subscription")
-                }
-        )
-        mCompositeDisposable.add(disposable)
-*/
     }
 
     fun openLoginActivity() {
@@ -134,24 +113,25 @@ class HomeActivityUI : AnkoComponent<HomeActivity>, AnkoLogger {
     lateinit var toolbar : Toolbar
     lateinit var drawerLayout: DrawerLayout
     lateinit var navigationView: NavigationView
-    lateinit var mUI: AnkoContext<HomeActivity>
-    private fun getHeaderView(view : View) : View {
-        return view.context.verticalLayout {
-            view.lparams(width = matchParent, height = wrapContent)
+    private fun createHeaderView(ui: AnkoContext<HomeActivity>, parent: ViewGroup) = with(parent.context) {
+        verticalLayout {
+            lparams(matchParent, wrapContent)
             backgroundColor = 0x3300FF00.toInt()
             gravity = Gravity.CENTER_HORIZONTAL
+
             button(R.string.hub_menu_login_button) {
                 onClick {
-                    mUI.owner.openLoginActivity()
+                    ui.owner.openLoginActivity()
+                    //手动控制drawerLayout的显示/隐藏
+                    drawerLayout.closeDrawer(Gravity.START)
                 }
-            }.lparams(width = wrapContent, height = wrapContent)
+            }.lparams(wrapContent, wrapContent)
             textView {
                 text = "Header View"
-            }.lparams(width = wrapContent, height = wrapContent)
+            }.lparams(wrapContent, wrapContent)
         }
     }
     override fun createView(ui: AnkoContext<HomeActivity>) = ui.apply {
-        mUI = ui
         drawerLayout = drawerLayout {
             lparams(width = matchParent, height = matchParent)
             verticalLayout {
@@ -173,7 +153,7 @@ class HomeActivityUI : AnkoComponent<HomeActivity>, AnkoLogger {
                 textView {
                     text = "content layout"
                     textSize = 24f
-                }.lparams(width = wrapContent, height = wrapContent)
+                }
                 button {
                     text = "test github api"
                     onClick {
@@ -183,11 +163,8 @@ class HomeActivityUI : AnkoComponent<HomeActivity>, AnkoLogger {
             }.lparams(width = matchParent, height = matchParent)
             navigationView = navigationView {
                 fitsSystemWindows = true
-//                inflateHeaderView(R.layout.hub_home_header)
-                addHeaderView(getHeaderView(this@navigationView))
+                addHeaderView(createHeaderView(ui, this@navigationView))
                 inflateMenu(R.menu.hub_home_menu)
-
-
             }.lparams(width = dip(240), height = matchParent) {
                 gravity = Gravity.START
             }
